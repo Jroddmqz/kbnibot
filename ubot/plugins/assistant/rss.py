d7314f4faf11476eddc58e379d22de65bb36e8ea
Jroddmqz <js.rdlf.mrqz.snts3@gmail.com>
2023-05-11 22:23:32 -0600

    Update rss.py

--- ubot/plugins/assistant/rss.py
+++ ubot/plugins/assistant/rss.py
@@ -18,108 +18,13 @@ if not os.path.exists(temp):
     os.makedirs(temp)
 
 
-@bot.on_message(filters.command(["rss"], prefixes=[".", "/"]) & filters.incoming)
-async def rss(client, message):
-
-    def get_feed_entries(url):
-        entries = []
-        feed = feedparser.parse(url)
-        for entry in feed.entries:
-            entry_data = {
-                "title": entry.title,
-                "link": entry.link,
-                "updated": entry.updated,
-                "updated_parsed": entry.updated_parsed,
-                "author": entry.author,
-            }
-
-            if "danbooru" in url:
-                entry_data = {
-                    "title": entry.title,
-                    "link": entry.link,
-                    "updated": entry.updated,
-                    "updated_parsed": entry.updated_parsed,
-                    "author": entry.author,
-                }
-            elif "lolibooru" in url:
-                entry_data = {
-                    "title": entry.title,
-                    "link": entry.link,
-                    "updated": entry.updated,
-                    "updated_parsed": entry.updated_parsed,
-                    "author": entry.author,
-                }
-            else:
-                entry_data = {
-                    "title": entry.title,
-                    "link": entry.link,
-                    "updated": entry.updated,
-                    "updated_parsed": entry.updated_parsed,
-                    "author": entry.author,
-                }
-            entries.append(entry_data)
-        return entries
-
-    def get_recent_entries2(url):
-        entries = get_feed_entries(url)
-        recent_entries = []
-        current_time = time.time()
-        for entry_ in entries:
-            published_time = parse(entry_["updated"]).timestamp()
-            if (current_time - published_time) < 86400:
-                recent_entries.append(entry_)
-        return recent_entries
-
-    def get_recent_entries(url, last_checked_time=None):
-        entries = get_feed_entries(url)
-        recent_entries = []
-        current_time = time.time()
-        if last_checked_time is None:
-            last_checked_time = current_time
-        for entry_ in entries:
-            published_time = parse(entry_["updated"]).timestamp()
-            print(f"last_checked_time {last_checked_time}")
-            print(f"published_time {published_time}")
-            print(f"current_time {current_time}")
-            if last_checked_time < published_time <= current_time:
-                print("agregando")
-                recent_entries.append(entry_)
-        return recent_entries, current_time
-
-    rss_url = message.text.split(' ', 1)[1]
-
-    b = Bruteforce()
-    last_checked_time = time.time()
-    while True:
-        #entries = get_recent_entries2(rss_url)
-        entries, last_checked_time = get_recent_entries(rss_url, last_checked_time)
-        print(f"entries:{entries}")
-        for entry in entries:
-            print(entry["title"], entry["link"])
-            try:
-                archive = b.danbooru(entry['link'])
-                response = requests.get(archive)
-                if response.status_code == 200:
-                    filename = os.path.basename(archive)
-                    with open(os.path.join(temp, filename), "wb") as f:
-                        f.write(response.content)
-                    print(f"Archivo {filename} descargado exitosamente.")
-
-                else:
-                    print("Error al descargar la imagen.")
-
-            except Exception as e:
-                logging.error("[KBNIBOT] - Failed To DownLoad : " + f"{str(e)}")
-
-        await asyncio.sleep(60)
-
-
 def get_feed_url():
     urls_ = []
     urls_.append(["https://danbooru.donmai.us/posts.atom", "-1001655727761"])
     urls_.append(["https://lolibooru.moe/post.atom", "-1001754747705"])
     urls_.append(["https://siftrss.com/f/9Mn0Rq9QgG", "-1001616034175"]) #@nyastrixroom
     urls_.append(["https://konachan.com/post/atom", "-1001850075721"]) #Konachan
+    urls_.append(["https://yande.re/post/atom?tags=", "@acgnxse"])
     return urls_
 
 global last_checked_time_g
@@ -129,7 +34,7 @@ for x in get_feed_url():
 last_checked_time_g = w
 
 
-@bot.on_message(filters.command(["ars"], prefixes=[".","/"]) & filters.incoming)
+@bot.on_message(filters.command(["rss"], prefixes=[".","/"]) & filters.incoming)
 async def ars(client, message):
     async def is_chat(item):
         try:
@@ -258,6 +163,8 @@ async def ars(client, message):
             elif 'konachan' in entry['link']:
                 archive = b.konachan(entry['link'])
                 archive = archive.replace(" ", "%20")
+            elif 'yande.re' in entry['link']:
+                archive = b.booru(entry['link'], site='yandere')
             else:
                 archive = ""
 
@@ -274,12 +181,12 @@ async def ars(client, message):
                 if ext_.lower() in {'.jpg', '.png', '.webp', '.jpeg'}:
                     new_file = resizer(f"{temp}{filename}")
                     try:
-                        await bot.send_photo(chatid, photo=new_file, caption=entry['title'])
+                        await app.send_photo(chatid, photo=new_file, caption=entry['title'])
                         await asyncio.sleep(1)
-                        await bot.send_document(chatid, document=f"{temp}{filename}")
+                        await app.send_document(chatid, document=f"{temp}{filename}")
                     except:
                         try:
-                            await bot.send_document(chatid, document=f"{temp}{filename}", caption=entry['title'])
+                            await app.send_document(chatid, document=f"{temp}{filename}", caption=entry['title'])
                         except Exception as e:
                             logging.error("[KBNIBOT] - Failed: " + f"{str(e)}")
 
@@ -287,17 +194,17 @@ async def ars(client, message):
                         os.remove(new_file)
                 elif ext_.lower() in {'.mp4', '.avi', '.mkv', '.mov'}:
                     try:
-                        await bot.send_video(chatid, video=f"{temp}{filename}", caption=entry['title'])
+                        await app.send_video(chatid, video=f"{temp}{filename}", caption=entry['title'])
                         await asyncio.sleep(1)
-                        await bot.send_document(chatid, document=f"{temp}{filename}")
+                        await app.send_document(chatid, document=f"{temp}{filename}")
                     except:
                         try:
-                            await bot.send_document(chatid, document=f"{temp}{filename}", caption=entry['title'])
+                            await app.send_document(chatid, document=f"{temp}{filename}", caption=entry['title'])
                         except Exception as e:
                             logging.error("[KBNIBOT] - Failed: " + f"{str(e)}")
                 else:
                     try:
-                        await bot.send_document(chatid, document=f"{temp}{filename}", caption=entry['title'])
+                        await app.send_document(chatid, document=f"{temp}{filename}", caption=entry['title'])
                     except Exception as e:
                         logging.error("[KBNIBOT] - Failed: " + f"{str(e)}")
 
@@ -317,4 +224,4 @@ async def ars(client, message):
 
         await asyncio.gather(*tasks)
         #print(last_checked_time_g)
-        await asyncio.sleep(60)
+        await asyncio.sleep(300)
