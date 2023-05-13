e8cb25c8c4547b18eaebc2885bb7f4c22b851fb0
Jroddmqz <js.rdlf.mrqz.snts3@gmail.com>
2023-05-13 17:03:10 -0600

    Update info.py

--- ubot/plugins/assistant/info.py
+++ ubot/plugins/assistant/info.py
@@ -1,10 +1,12 @@
+import os
+
 from pyrogram import filters
 
 from ubot import app, bot
 from ubot.config import Config
 
 
-@bot.on_message(filters.command(["info"], prefixes=[".","/"]) & filters.incoming)
+@bot.on_message(filters.command(["info"], prefixes=[".", "/"]) & filters.incoming)
 async def info(client, message):
     if message.reply_to_message:
         username = message.reply_to_message.from_user.username
@@ -13,8 +15,8 @@ async def info(client, message):
         user_link = message.reply_to_message.from_user.mention
 
         if message.reply_to_message.from_user.photo:
-            async for photo in app.get_chat_photos(message.reply_to_message.from_user.id,1):
-                photo_ = await app.download_media((photo.file_id))
+            async for photo in app.get_chat_photos(message.reply_to_message.from_user.id, 1):
+                photo_ = await app.download_media(photo.file_id)
         else:
             photo_ = Config.img_url
     else:
@@ -24,7 +26,7 @@ async def info(client, message):
         user_link = message.from_user.mention
 
         if message.from_user.photo:
-            async for photo in app.get_chat_photos(message.from_user.id,1):
+            async for photo in app.get_chat_photos(message.from_user.id, 1):
                 photo_ = await app.download_media(photo.file_id)
         else:
             photo_ = Config.img_url
@@ -41,3 +43,4 @@ ID: <code>{id}</code>
 First Name: {first_name}
 User link: {user_link}"""
     await bot.send_document(message.chat.id, document=photo_, caption=text)
+    os.remove(photo_)
