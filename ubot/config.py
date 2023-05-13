7727bd1ec976eb9a8f4152ea6518e392c2395d25
Jroddmqz <js.rdlf.mrqz.snts3@gmail.com>
2023-05-13 13:39:52 -0600

    Update config.py

--- ubot/config.py
+++ ubot/config.py
@@ -11,6 +11,7 @@ class Config(object):
     api_id = int(os.environ.get('API_ID'))
     api_hash = os.environ.get('API_HASH', None)
     bot_token = os.environ.get('BOT_TOKEN', None)
+    bot_token2 = os.environ.get('BOT_TOKEN2', None)
 
     document_root = os.environ.get('DOCUMENT_ROOT')
     log_group = int(os.environ.get('LOG_GROUP', False))
