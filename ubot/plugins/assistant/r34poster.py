db91c60b7af548621820ec3465b8b86f5b42efff
Jroddmqz <js.rdlf.mrqz.snts3@gmail.com>
2023-05-13 13:40:43 -0600

    Update r34poster.py

--- ubot/plugins/assistant/r34poster.py
+++ ubot/plugins/assistant/r34poster.py
@@ -1,53 +1,35 @@
-import os
-import time
-import math
 import asyncio
-import logging
-import requests
-import feedparser
-import random, string
-from pyrogram import filters
+import math
+import os
+import random
+import string
 from datetime import datetime
-from bs4 import BeautifulSoup
-from dateutil.parser import parse
-from ubot import app, bot, Mclient
-from ubot.utils.misc import resizer#, # is_chat
-from ubot.utils.san import Bruteforce
-from pymongo import ASCENDING, DESCENDING
 
+import requests
+from bs4 import BeautifulSoup
+from pymongo import DESCENDING
+from pyrogram import filters
+
+from ubot import app, bot, bot2, Mclient
+from ubot.utils.misc import upload_from_queue, is_chat
+
+if bot2:
+    uploader = bot2
 
 temp = '.temp/'
 if not os.path.exists(temp):
     os.makedirs(temp)
 
 
-@bot.on_message(filters.command(["r34"], prefixes=[".","/"]) & filters.incoming)
+@bot.on_message(filters.command(["r34"], prefixes=[".", "/"]) & filters.incoming)
 async def r34(client, message):
 
-    async def is_chat(item):
-        try:
-            chat_id = int(item)
-            try:
-                chat = await app.get_chat(chat_id)
-            except:
-                return None
-            chat_id = chat.id
-        except ValueError:
-            if not item.startswith("@"):
-                return None
-            try:
-                chat = await app.get_chat(item)
-            except:
-                return None
-            chat_id = chat.id
-        return chat_id
     async def get_rule():
-        rule = []
-        rule.append(["honkai:_star_rail", "-1001407356398", "😈 https://t.me/+4wqeR0dRySJlMDUx"]) #leeching group
+        rule=[]
+        rule.append(["honkai:_star_rail", "-1001407356398", "😈 https://t.me/+4wqeR0dRySJlMDUx"])  # leeching group
         rule.append(["genshin_impact", "-1001629528057", "😈 https://t.me/+45f-VAxVB2NjN2Jh"])
-        #url.append(["", ""])
+        # url.append(["", ""])
         return rule
-
 
     async def process(rule):
         api_rule_url = "https://api.rule34.xxx/index.php?page=dapi&s=post&q=index&tags="
@@ -69,22 +51,22 @@ async def r34(client, message):
         collection = db[f'{rule[0]}']
 
         for x in post:
-            item = {"id": x.get('id'), "file_url": x.get('file_url'), "source": x.get('source'), "published": False}
+            item = {"id": x.get('id'), "file_url": x.get('file_url'), "source": x.get('source'), "tag": rule[0], "published": False}
             exist = collection.find_one({"id": x.get('id')})
             if not exist:
                 collection.insert_one(item)
 
-        chatid = await is_chat(rule[1])
-        if chatid is None:
-            print(f"error chat id {chatid}")
+        _chat_id = await is_chat(app, rule[1])
+        if _chat_id is None:
+            print(f"error chat id {_chat_id}")
             return
 
-        print(f"{rule[0]} --- {count}items --- Posting to{chatid}")
+        print(f"{rule[0]} --- {count}items --- Posting to{_chat_id}")
 
         items = collection.find().sort([("$natural", DESCENDING)])
 
         for x in items:
-            if x['published'] == False:
+            if not x['published']: #if x['published'] == False:
                 archive = x['file_url']
                 response = requests.get(archive)
                 if response.status_code == 200:
@@ -98,57 +80,20 @@ async def r34(client, message):
                         f.write(response.content)
 
                     url = x['source']
-                    value = f"[Sauce]({url})"
-                    #value = f"<a href='{url}'>Sauce</a>"
-                    capy = value + "\n" + rule[2]
+                    source = f"[source]({url})"
+                    capy = source + "\n" + rule[2]
+                    filepath = f"{temp}{filename}"
 
-                    if ext_.lower() in {'.jpg', '.png', '.webp', '.jpeg'}:
-                        new_file = resizer(f"{temp}{filename}")
-                        try:
-                            sended = await app.send_photo(chatid, photo=new_file, caption=str(capy))
-                            await asyncio.sleep(1)
-                            await app.send_document(chatid, document=f"{temp}{filename}")
-                        except:
-                            try:
-                                sended = await app.send_document(chatid, document=f"{temp}{filename}", caption=str(capy))
-                            except Exception as e:
-                                logging.error("[KBNIBOT] - Failed: " + f"{str(e)}")
+                    await queue.put((uploader, filepath, _chat_id, capy, ext_, x))
 
-                        if os.path.exists(new_file):
-                            os.remove(new_file)
-                    elif ext_.lower() in {'.mp4', '.avi', '.mkv', '.mov'}:
-                        try:
-                            sended = await app.send_video(chatid, video=f"{temp}{filename}", caption=str(capy))
-                            await asyncio.sleep(1)
-                            await app.send_document(chatid, document=f"{temp}{filename}")
-                        except:
-                            try:
-                                sended = await app.send_document(chatid, document=f"{temp}{filename}", caption=str(capy))
-                            except Exception as e:
-                                logging.error("[KBNIBOT] - Failed: " + f"{str(e)}")
-                    else:
-                        try:
-                            sended = await app.send_document(chatid, document=f"{temp}{filename}", caption=str(capy))
-                        except Exception as e:
-                            logging.error("[KBNIBOT] - Failed: " + f"{str(e)}")
+        upload_task = asyncio.create_task(upload_from_queue(queue))
 
-                    if sended != None:
-                        if not x['published']:
-                            filter = {'id': x['id']}
-                            update = {'$set': {'published': True}}
-                            result = collection.update_one(filter, update)
-
-
-                    os.remove(f"{temp}{filename}")
-                    await asyncio.sleep(60)
-
-
-
+    queue = asyncio.Queue()
     db = Mclient["rule"]
     ruler = await get_rule()
+
     while True:
         tasks = [asyncio.create_task(process(rule)) for rule in ruler]
-
         await asyncio.gather(*tasks)
-        #print(last_checked_time_g)
+        await queue.join()
         await asyncio.sleep(86400)
