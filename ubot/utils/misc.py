951ba2e67ea2c48e8a2afa36741d5452783661e2
Jroddmqz <js.rdlf.mrqz.snts3@gmail.com>
2023-05-15 21:12:27 -0600

    Update misc.py

--- ubot/utils/misc.py
+++ ubot/utils/misc.py
@@ -1,16 +1,15 @@
-import itertools
 import os
-import pathlib
-
+import time
 import cv2
-import imageio
-import requests
-from PIL import Image
-from bs4 import BeautifulSoup
-from moviepy.video.io.VideoFileClip import VideoFileClip
-import asyncio
-from ubot import app, bot, Mclient
 import logging
+import imageio
+import asyncio
+import pathlib
+import itertools
+from PIL import Image
+from ubot import app, bot, Mclient, log_group
+from moviepy.video.io.VideoFileClip import VideoFileClip
+
 
 
 def item_for_search():
@@ -138,49 +137,55 @@ async def is_chat(client, item):
 
 
 async def upload_file(client, file_path, chat_id, capy, ext_, x_item=False):
-    print(f"{x_item['file_url']} -- {file_path}")
-    if ext_.lower() in {'.jpg', '.png', '.webp', '.jpeg'}:
-        new_file = resizer(file_path)
-        try:
-            sended = await client.send_photo(chat_id, photo=new_file, caption=str(capy))
-            await asyncio.sleep(1)
-            await client.send_document(chat_id, document=file_path)
-        except:
+    _sent_ = None
+    try:
+        if ext_.lower() in {'.jpg', '.png', '.webp', '.jpeg'}:
+            new_file = resizer(file_path)
             try:
-                sended = await client.send_document(chat_id, document=file_path, caption=str(capy))
+                _sent_ = await client.send_photo(chat_id, photo=new_file, caption=str(capy))
+                await asyncio.sleep(1)
+                await client.send_document(chat_id, document=file_path)
+            except:
+                try:
+                    _sent_ = await client.send_document(chat_id, document=file_path, caption=str(capy))
+                except Exception as e:
+                    logging.error("[KBNIBOT] - Failed: " + f"{str(e)}")
+            if os.path.exists(new_file):
+                os.remove(new_file)
+        elif ext_.lower() in {'.mp4', '.avi', '.mkv', '.mov'}:
+            try:
+                _sent_ = await client.send_video(chat_id, video=file_path, caption=str(capy))
+                await asyncio.sleep(1)
+            except:
+                try:
+                    _sent_ = await client.send_document(chat_id, document=file_path, caption=str(capy))
+                except Exception as e:
+                    logging.error("[KBNIBOT] - Failed: " + f"{str(e)}")
+        else:
+            try:
+                _sent_ = await client.send_document(chat_id, document=file_path, caption=str(capy))
             except Exception as e:
                 logging.error("[KBNIBOT] - Failed: " + f"{str(e)}")
-        if os.path.exists(new_file):
-            os.remove(new_file)
-    elif ext_.lower() in {'.mp4', '.avi', '.mkv', '.mov'}:
-        try:
-            sended = await client.send_video(chat_id, video=file_path, caption=str(capy))
-            await asyncio.sleep(1)
-            await client.send_document(chat_id, document=file_path)
-        except:
-            try:
-                sended = await client.send_document(chat_id, document=file_path, caption=str(capy))
-            except Exception as e:
-                logging.error("[KBNIBOT] - Failed: " + f"{str(e)}")
-    else:
-        try:
-            sended = await client.send_document(chat_id, document=file_path, caption=str(capy))
-        except Exception as e:
+
+        os.remove(file_path)
+
+        if x_item is None:
+            pass
+        else:
+            db = Mclient["rule"]
+            collect = db[f"{x_item['tag']}"]
+            if _sent_ is not None:
+                if not x_item['published']:
+                    fil_ter = {'id': x_item['id']}
+                    update = {'$set': {'published': True}}
+                    collect.update_one(fil_ter, update)
+    except Exception as e:
+        if "[420 FLOOD_WAIT_X]" in str(e):
+            print(f"str(e)-{str(e)}-")
+            print('Flood: Wait for', int(str(e).split()[5]), 'seconds')
+            time.sleep(int(str(e).split()[5]))
+        else:
             logging.error("[KBNIBOT] - Failed: " + f"{str(e)}")
-
-    os.remove(file_path)
-
-    if x_item is None:
-        pass
-    else:
-        db = Mclient["rule"]
-        collect = db[f"{x_item['tag']}"]
-        if sended != None:
-            if not x_item['published']:
-                filter = {'id': x_item['id']}
-                update = {'$set': {'published': True}}
-                collect.update_one(filter, update)
-
 
 async def upload_from_queue(queue):
     while True:
@@ -190,51 +195,3 @@ async def upload_from_queue(queue):
             await upload_file(client, file_path, chat_id, capy, ext_, x)
             queue.task_done()
         await asyncio.sleep(1)
-
-
-async def get_tags_rule34xxx(_id_):
-    url = f"https://rule34.xxx/index.php?page=post&s=view&id={_id_}"
-    headers = {
-        "User-Agent": "Mi Agente de Usuario Personalizado",
-        "Accept-Language": "es-ES,es;q=0.9,en;q=0.8"
-    }
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, "html.parser")
-
-        c = soup.find_all("li", class_="tag-type-character tag")
-        character_ = []
-        for li in c:
-            a_tag = li.find_all_next("a", href=True)
-            character_.append(a_tag[1].text)
-
-        q = soup.find_all("li", class_="tag-type-copyright tag")
-        copyright_ = []
-        for li in q:
-            a_tag = li.find_all_next("a", href=True)
-            copyright_.append(a_tag[1].text)
-
-        a = soup.find_all("li", class_="tag-type-artist tag")
-        artist_ = []
-        for li in a:
-            a_tag = li.find_all_next("a", href=True)
-            artist_.append(a_tag[1].text)
-    else:
-        print("Error al hacer la solicitud:", response.status_code)
-
-    char_txt = ""
-    for x in character_:
-        char_txt = f"{char_txt} {x} "
-
-    brand_txt = ""
-    for x in copyright_:
-        brand_txt = f"{brand_txt} {x} "
-
-    artist_txt = ""
-    for x in artist_:
-        artist_txt = f"{artist_txt} {x} "
-
-    txt = f"CHAR: {char_txt}\nBRAND: {brand_txt}\nARTIST: {artist_txt}"
-
-    return txt
