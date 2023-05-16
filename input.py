4c1fc40cfc23a6202fc244d5129f9dc17736f29e
Jroddmqz <js.rdlf.mrqz.snts3@gmail.com>
2023-05-15 21:12:14 -0600

    Create input.py

---
+++ input.py
@@ -0,0 +1,27 @@
+rss = [
+    {
+        "rss_url": "https://danbooru.donmai.us/posts.atom",
+        "channel": "-1001655727761",
+        "caption": ""
+    },
+    {
+        "rss_url": "https://lolibooru.moe/post.atom",
+        "channel": "-1001754747705",
+        "caption": ""
+    },
+    {  # @nyastrixroom
+        "rss_url": "https://siftrss.com/f/9Mn0Rq9QgG",
+        "channel": "-1001616034175",
+        "caption": ""
+    },
+    {  # Konachan
+        "rss_url": "https://konachan.com/post/atom",
+        "channel": "-1001850075721",
+        "caption": ""
+    },
+    {
+        "rss_url": "https://yande.re/post/atom?tags=",
+        "channel": "@acgnxse",
+        "caption": ""
+    },
+]
